fe631449f6d9838bcebf4c4a8b230a322cd2351f
Mohammed abdelkafi <kawruusir@gmail.com>
2025-10-09 01:39:37 +0200

    Update memory_manager.c

--- kernel/memory_manager.c
+++ kernel/memory_manager.c
@@ -1,9 +1,16 @@
 #include "memory_manager.h"
-
 uint32_t total_bytes_allocated = 0;
-
+typedef struct {
+    uint32_t length;
+    uint32_t active;
+} Block;
 void* malloc(uint32_t length)
 {
     total_bytes_allocated+=length;
     return (void*)((uint32_t)(MALLOC_MEMORY_LOCATION) + (uint32_t)((total_bytes_allocated-length)));
+}
+void* free(void* Location)
+{
+    Block* freemyboy = ((Block*)Location) - 1;
+    freemyboy->active = 1;
 }
